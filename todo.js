6a8ae240da793535dbd05fe1dcab232af2fa03a0
Odera5 <anikingsleyodera5@gmail.com>
2025-03-31 20:47:05 +0100

    my last commit

--- todo.js
+++ todo.js
@@ -26,91 +26,95 @@ main.appendChild(showProgress);
 
 let count = 0;
 function createTodo() {
-  const todoContainer = document.createElement("div");
-  todoContainer.classList.add("todoContainer");
-  main.appendChild(todoContainer);
-  //todoContainer.textContent = input1.value;
-  // todoContainer.contentEditable = "true";
+  if (input1.value == "") {
+    event.preventDefault();
+  } else {
+    const todoContainer = document.createElement("div");
+    todoContainer.classList.add("todoContainer");
+    main.appendChild(todoContainer);
+    //todoContainer.textContent = input1.value;
+    // todoContainer.contentEditable = "true";
 
-  const todoList = document.createElement("p");
-  todoList.classList.add("todoList");
-  todoList.textContent = input1.value;
-  const radio = document.createElement("input");
-  radio.type = "radio";
-  radio.id = "radioID";
-  todoContainer.appendChild(radio);
+    const todoList = document.createElement("p");
+    todoList.classList.add("todoList");
+    todoList.textContent = input1.value;
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.id = "radioID";
+    todoContainer.appendChild(radio);
 
-  const BtnContainer = document.createElement("div");
-  BtnContainer.classList.add("BtnContainer");
-  const editBtn = document.createElement("button");
-  editBtn.style.backgroundImage = "url(./pencil.svg)";
-  const deleteBtn = document.createElement("button");
-  editBtn.classList.add("editBtn");
-  deleteBtn.classList.add("deleteBtn");
-  todoContainer.appendChild(todoList);
-  todoContainer.appendChild(BtnContainer);
-  BtnContainer.appendChild(editBtn);
-  BtnContainer.appendChild(deleteBtn);
-  input1.value = "";
-
-  editBtn.addEventListener("click", () => {
-    BtnContainer.removeChild(editBtn);
-    const checkBtn = document.createElement("button");
-    checkBtn.style.backgroundImage = "url(./check.svg)";
-    BtnContainer.appendChild(checkBtn);
+    const BtnContainer = document.createElement("div");
+    BtnContainer.classList.add("BtnContainer");
+    const editBtn = document.createElement("button");
+    editBtn.style.backgroundImage = "url(./pencil.svg)";
+    const deleteBtn = document.createElement("button");
+    editBtn.classList.add("editBtn");
+    deleteBtn.classList.add("deleteBtn");
+    todoContainer.appendChild(todoList);
+    todoContainer.appendChild(BtnContainer);
+    BtnContainer.appendChild(editBtn);
     BtnContainer.appendChild(deleteBtn);
-    checkBtn.classList.add("checkBtn");
-    checkBtn.addEventListener("click", () => {
-      todoList.contentEditable = "false";
-      BtnContainer.removeChild(checkBtn);
-      BtnContainer.appendChild(editBtn);
+    input1.value = "";
+
+    editBtn.addEventListener("click", () => {
+      BtnContainer.removeChild(editBtn);
+      const checkBtn = document.createElement("button");
+      checkBtn.style.backgroundImage = "url(./check.svg)";
+      BtnContainer.appendChild(checkBtn);
       BtnContainer.appendChild(deleteBtn);
+      checkBtn.classList.add("checkBtn");
+      checkBtn.addEventListener("click", () => {
+        todoList.contentEditable = "false";
+        BtnContainer.removeChild(checkBtn);
+        BtnContainer.appendChild(editBtn);
+        BtnContainer.appendChild(deleteBtn);
+      });
+
+      todoList.contentEditable = "true";
+      todoList.focus();
+      let range = document.createRange();
+      let selection = window.getSelection();
+
+      range.selectNodeContents(todoList);
+      range.collapse(false);
+      selection.removeAllRanges();
+      selection.addRange(range);
     });
+    deleteBtn.addEventListener("click", () => {
+      main.removeChild(todoContainer);
+      count--;
 
-    todoList.contentEditable = "true";
-    todoList.focus();
-    let range = document.createRange();
-    let selection = window.getSelection();
-
-    range.selectNodeContents(todoList);
-    range.collapse(false);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  });
-  deleteBtn.addEventListener("click", () => {
-    main.removeChild(todoContainer);
-    count--;
-
-    if (count == 0) {
-      showProgress.textContent = "";
-      count = 0;
-    } else if (count < 0) {
-      count = 0;
-    } else if (count > 0) {
-      count = count;
-    }
-    console.log(count);
-    /*showProgress.textContent = `Yayy! you have completed ${count} task. `;
+      if (count == 0) {
+        showProgress.textContent = "";
+        count = 0;
+      } else if (count < 0) {
+        count = 0;
+      } else if (count > 0) {
+        count = count;
+      }
+      console.log(count);
+      /*showProgress.textContent = `Yayy! you have completed ${count} task. `;
     setTimeout(() => {
       showProgress.textContent = "";
     }, 3000);*/
-  });
+    });
 
-  radio.addEventListener(
-    "click",
-    () => {
-      count++;
-      console.log(count);
-      BtnContainer.removeChild(editBtn);
-      todoList.style.textDecoration = "line-through";
+    radio.addEventListener(
+      "click",
+      () => {
+        count++;
+        console.log(count);
+        BtnContainer.removeChild(editBtn);
+        todoList.style.textDecoration = "line-through";
 
-      showProgress.textContent = `Yayy! you have completed ${count} task. grab your self some shawarma `;
-      setTimeout(() => {
-        showProgress.textContent = "";
-      }, 3000);
-    },
-    { once: true }
-  );
+        showProgress.textContent = `Yayy! you have completed ${count} task. grab your self some shawarma `;
+        setTimeout(() => {
+          showProgress.textContent = "";
+        }, 3000);
+      },
+      { once: true }
+    );
+  }
 }
 
 addBtn.addEventListener("click", createTodo);
